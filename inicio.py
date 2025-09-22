25d64413d43de8e0a93592eac4369fc192e25f9f
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2025-09-22 04:30:51 -0500

    feat(inicio): agregar función para limpiar pantalla y mejorar la navegación del menú

--- inicio.py
+++ inicio.py
@@ -1,4 +1,7 @@
+import sys
+
 from utils.convertir_entrada import convertir_entrada
+from utils.limpiar_pantalla import limpiar_pantalla
 from m4a_mp3 import convertir_m4a_mp3
 from m4a_opus import convertir_m4a_opus
 from mp4_mp3 import convertir_mp4_mp3
@@ -24,29 +27,34 @@ def menu():
             ruta_entrada, ruta_salida = convertir_entrada("opus")
             convertir_m4a_opus(ruta_entrada, ruta_salida)
             input("\nPresione Enter para continuar...")
+            limpiar_pantalla()
 
         elif opcion == "2":
             ruta_entrada, ruta_salida = convertir_entrada("mp3")
             convertir_m4a_mp3(ruta_entrada, ruta_salida)
             input("\nPresione Enter para continuar...")
+            limpiar_pantalla()
         
         elif opcion == "3":
             ruta_entrada, ruta_salida = convertir_entrada("mp4")
             convertir_m4aopus_mp4mp3(ruta_entrada, ruta_salida)
             input("\nPresione Enter para continuar...")
+            limpiar_pantalla()
 
         elif opcion == "4":
             ruta_entrada, ruta_salida = convertir_entrada("mp3")
             convertir_mp4_mp3(ruta_entrada, ruta_salida)
             input("\nPresione Enter para continuar...")
+            limpiar_pantalla()
 
         elif opcion == "5":
             print("Saliendo...")
-            exit()
+            sys.exit()
 
         else:
             print("Opción no válida. Intente de nuevo.")
             input("\nPresione Enter para continuar...")
+            limpiar_pantalla()
 
 
 # Punto de partida del programa
